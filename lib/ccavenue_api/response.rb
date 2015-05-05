1d958206ed73811bf711608c4bb49715f2a1930c
Chetan Patil <cpatil@gmail.com>
2015-05-05 16:15:29 -0700

    fixed issue with CCAvenue validation criterion

--- lib/ccavenue_api/response.rb
+++ lib/ccavenue_api/response.rb
@@ -23,16 +23,16 @@ module CcavenueApi
           decrypted_hash = ActiveSupport::JSON.decode(decrypted_payload)
           parsed         = build_from_response(decrypted_hash)
           self.new({
-                       :http_status      => :success,
-                       :api_status       => :success,
-                       :original_payload => decrypted_hash
+                     :http_status      => :success,
+                     :api_status       => :success,
+                     :original_payload => decrypted_hash
                    }.merge(parsed))
         end
       end
 
       # the keys of this hash are the attributes of the response
       def build_from_response(response)
-        hsh = if response['Refund_Order_Result']  # refund response
+        hsh = if response['Refund_Order_Result'] # refund response
                 tmp = {refund_status: (check_if_equal(response['Refund_Order_Result']['refund_status'], 0)) ? :success : :failed, }
                 if response['Refund_Order_Result']['reason'] # Only present for failed requests
                   tmp[:reason] = response['Refund_Order_Result']['reason']
@@ -41,13 +41,13 @@ module CcavenueApi
                 # "{\"Order_Status_Result\":{\"status\":1,\"error_desc\":\"Providing Reference_No/Order No is mandatory.\"}}"
               elsif response['Order_Status_Result'] # order status response
                 tmp          = {
-                    request_status:         (check_if_equal(response['Order_Status_Result']['status'], 0)) ? :success : :failed,
-                    order_status:           (check_if_equal(response['Order_Status_Result']['order_status'], 0)) ? :success : :failed,
-                    order_status_date_time: response['Order_Status_Result']['order_status_date_time']
+                  request_status:         (check_if_equal(response['Order_Status_Result']['status'], 0)) ? :success : :failed,
+                  order_status:           (check_if_equal(response['Order_Status_Result']['order_status'], 0)) ? :success : :failed,
+                  order_status_date_time: response['Order_Status_Result']['order_status_date_time']
                 }
                 tmp[:reason] = response['Order_Status_Result']['error_desc'] if response['Order_Status_Result']['error_desc']
                 tmp
-              elsif response['Order_Result']  # cancel response
+              elsif response['Order_Result'] # cancel response
                 success_count = Integer(response['Order_Result']['success_count']) rescue nil
                 tmp = {success_count: success_count}
                 if response['Order_Result']['failed_List'] && (response['Order_Result']['failed_List']['failed_order']).kind_of?(Hash)
@@ -64,8 +64,8 @@ module CcavenueApi
       rescue => e
         Rails.logger.error("Error parsing ccavenue api response: #{e.message}")
         return {
-            reason:     Spree.t("ccavenue.api_response_parse_failed"),
-            api_status: :failed
+          reason:     Spree.t("ccavenue.api_response_parse_failed"),
+          api_status: :failed
         }
       end
 
@@ -121,11 +121,16 @@ module CcavenueApi
     end
 
     ### merchant credentials validation
-    VALIDATION_SUCCESS_MSG = 'Providing Reference_No/Order No is mandatory'
+    VALIDATION_SUCCESS_MSG = ['Providing Reference_No/Order No is mandatory',
+                              'Providing Reference number/Order Number is mandatory']
 
     def credentials_valid?
-      valid_reason = @reason.match(/#{VALIDATION_SUCCESS_MSG}/) unless @reason.blank?
-      self.http_status == :success && self.api_status == :success && valid_reason.present?
+      valid_reason = if @reason.blank?
+                       false
+                     else
+                       VALIDATION_SUCCESS_MSG.include?(@reason)
+                     end
+      self.http_status == :success && self.api_status == :success && valid_reason
     end
 
     def credentials_validation_error
